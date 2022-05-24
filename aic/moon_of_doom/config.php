77b4fe8d808d6f5df638d1fa8d4c6d7987b67021
Lucas Kalani Ross <61758874+lucassso@users.noreply.github.com>
2022-05-23 20:19:51 -0700

    haha funny css bc bored

--- aic/moon_of_doom/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// if you steal my pwd ISTG
-define('DB_DSN', "mysql:host=localhost;dbname=lross");
-define('DB_USER', "lross");
-define('DB_PASSWORD', "KCP69121");
- ?>
--- aic/moon_of_doom/game.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(()=>{
-  // $("div.footer form input[type='submit']").on("click", (event)=>{
-  //   $("div.game").remove();
-  //   $("div.text").remove();
-  // });
-});
